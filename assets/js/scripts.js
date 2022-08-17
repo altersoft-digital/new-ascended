8132d68e13a56ff802815c1e733b94606d7fdc71
Kenji <vishlano.mivali@gmail.com>
2022-08-17 13:35:36 +0400

    lkjlk

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,19 @@
 function scrollToTop() {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 }
 
+let btn = document.querySelector("#btn-scrolltop");
 
-let btn = document.querySelector('#btn-scrolltop');
-
-
-document.addEventListener('scroll', function(e) {
-    if (window.scrollY > 300) {
-        btn.style.display = "grid";
-    } else {
-        btn.style.display = "none";
-    }
+document.addEventListener("scroll", function (e) {
+  if (window.scrollY > 300) {
+    btn.style.display = "grid";
+  } else {
+    btn.style.display = "none";
+  }
 });
 
-// function switchTheme() {
-//     document.body.classList.add("dark-bg");
-//     document.querySelector('h1').classList.add('white-text');
-//     document.querySelector
-// }
+function switchTheme() {
+  document.body.classList.add("dark-bg");
+  document.querySelector("h1").classList.add("white-text");
+  // document.querySelector
+}
